678f2b327ee1369f999c5ea6b46d2242fcb3888f
krzys <krzys@DEARWOLF>
2024-05-27 20:01:56 +0200

    Dodaj pliki projektów.

---
+++ Gopher toolbox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gopher_toolbox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
